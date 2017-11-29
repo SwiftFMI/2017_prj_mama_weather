31bf1df00a7578dd9147ff98a7f4515184dc7230
Boyan Vushkov <bvushkov@gmail.com>
2017-11-29 15:43:54 +0200

    Initial Commit

---
+++ MamaWeatherTests/MamaWeatherTests.swift
@@ -0,0 +1,36 @@
+//
+//  MamaWeatherTests.swift
+//  MamaWeatherTests
+//
+//  Created by Boyan Vushkov on 29.11.17.
+//  Copyright © 2017 Boyan Vushkov. All rights reserved.
+//
+
+import XCTest
+@testable import MamaWeather
+
+class MamaWeatherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
