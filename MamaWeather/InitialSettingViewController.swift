7798c85b425dc53ea002c2ec3ee5d3660591052c
Maria Bozhkova <maria.bozhkova@yahoo.com>
2018-01-14 18:34:06 +0200

    Add some view controllers

---
+++ MamaWeather/InitialSettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InitialSettingViewController.swift
+//  MamaWeather
+//
+//  Created by Maria Bozhkova on 1/14/18.
+//  Copyright © 2018 Boyan Vushkov. All rights reserved.
+//
+
+import UIKit
+
+class InitialSettingViewController: UIViewController {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var age: UITextField!
+    
+    @IBAction func TapLeftButtonGesture(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @IBAction func TapRightButtonGesture(_ sender: UITapGestureRecognizer) {
+    }
+    
+}
